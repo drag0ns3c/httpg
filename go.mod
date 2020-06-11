0dacff9ef3a0ccb53222758326f22d792bba29cb
drag0ns3c <drag0ns3c@protonmail.com>
2020-06-11 08:24:08 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/drag0ns3c/httpg
+
+go 1.14
+
+require (
+	github.com/gobuffalo/envy v1.9.0 // indirect
+	github.com/gobuffalo/packd v1.0.0 // indirect
+	github.com/gobuffalo/packr v1.30.1
+	github.com/gorilla/mux v1.7.4
+	github.com/rogpeppe/go-internal v1.6.0 // indirect
+)
---
+++ httpg.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/gobuffalo/packr"
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func StaticHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	box := packr.NewBox("./static")
+	asset, err := box.FindString(vars["asset"])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(asset))
+}
+
+type SystemInfo struct {
+	OS string
+}
+
+func SystemInfoHandler(w http.ResponseWriter, r *http.Request) {
+	box := packr.NewBox("./templates")
+	index, err := box.FindString("index.html")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	tmpl := template.New("")
+	tmpl.Parse(index)
+	err = tmpl.Execute(w, &SystemInfo{OS: "mac"})
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	var wait time.Duration
+	var port string
+	var host string
+
+	flag.StringVar(&port, "port", "8080", "the port to listen on")
+	flag.StringVar(&host, "host", "0.0.0.0", "the IP to listen on")
+	flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", SystemInfoHandler)
+	r.HandleFunc("/static/{asset}", StaticHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("%s:%s", host, port),
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler: r, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+}
